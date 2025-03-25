5ec925c40b223486418bff775d4d69600513dfe8
Lucas <lukasmaru27@gmail.com>
2025-03-24 21:55:36 -0300

    Esquleto
    Cambio el esqueleto a la versión funcional que subio el profe

--- Ejercicios en clase/seno_ruido.py
+++ Ejercicios en clase/seno_ruido.py
@@ -113,6 +113,8 @@ plt.close('all')
 
 plt.figure(1)
 
+plt.plot(tt, srq, lw=2, linestyle='', color='blue', marker='o', markersize=5, markerfacecolor='blue', markeredgecolor='blue', fillstyle='none', label='ADC out (diezmada)')
+plt.plot(tt, sr, lw=1, color='black', marker='x', ls='dotted', label='$ s $ (analog)')
 
 plt.title('Señal muestreada por un ADC de {:d} bits - $\pm V_R= $ {:3.1f} V - q = {:3.3f} V'.format(B, Vf, q) )
 plt.xlabel('tiempo [segundos]')
@@ -121,30 +123,33 @@ axes_hdl = plt.gca()
 axes_hdl.legend()
 plt.show()
 
-#%% 
+
+###########
+# Espectro
+###########
 
 plt.figure(2)
-ft_SR = 1/N*np.fft.fft( sr, axis = 0 )
-ft_Srq = 1/N*np.fft.fft( srq, axis = 0 )
-ft_As = 1/N*np.fft.fft( analog_sig, axis = 0)
-ft_Nq = 1/N*np.fft.fft( nq, axis = 0 )
-ft_Nn = 1/N*np.fft.fft( nn, axis = 0 )
+ft_SR = 1/N*np.fft.fft( sr)
+ft_Srq = 1/N*np.fft.fft( srq)
+ft_As = 1/N*np.fft.fft( analog_sig)
+ft_Nq = 1/N*np.fft.fft( nq)
+ft_Nn = 1/N*np.fft.fft( nn)
 
 # grilla de sampleo frecuencial
 ff = np.linspace(0, (N-1)*df, N)
 
-bfrec = ff <= fs/2 #valores entre cero y nyquist
+bfrec = ff <= fs/2
 
 Nnq_mean = np.mean(np.abs(ft_Nq)**2)
 nNn_mean = np.mean(np.abs(ft_Nn)**2)
 
 plt.plot( ff[bfrec], 10* np.log10(2*np.abs(ft_As[bfrec])**2), color='orange', ls='dotted', label='$ s $ (sig.)' )
 plt.plot( np.array([ ff[bfrec][0], ff[bfrec][-1] ]), 10* np.log10(2* np.array([nNn_mean, nNn_mean]) ), '--r', label= '$ \overline{n} = $' + '{:3.1f} dB'.format(10* np.log10(2* nNn_mean)) )
-#plt.plot( ff[bfrec], 10* np.log10(2*np.mean(np.abs(ft_SR)**2, axis=1)[bfrec]), ':g', label='$ s_R = s + n $' )
-plt.plot( ff[bfrec], 10* np.log10(2*np.mean(np.abs(ft_Srq)**2, axis=1)[bfrec]), lw=2, label='$ s_Q = Q_{B,V_F}\{s_R\}$' )
+plt.plot( ff[bfrec], 10* np.log10(2*np.abs(ft_SR[bfrec])**2), ':g', label='$ s_R = s + n $' )
+plt.plot( ff[bfrec], 10* np.log10(2*np.abs(ft_Srq[bfrec])**2), lw=2, label='$ s_Q = Q_{B,V_F}\{s_R\}$' )
 plt.plot( np.array([ ff[bfrec][0], ff[bfrec][-1] ]), 10* np.log10(2* np.array([Nnq_mean, Nnq_mean]) ), '--c', label='$ \overline{n_Q} = $' + '{:3.1f} dB'.format(10* np.log10(2* Nnq_mean)) )
-plt.plot( ff[bfrec], 10* np.log10(2*np.mean(np.abs(ft_Nn)**2, axis=1)[bfrec]), ':r')
-plt.plot( ff[bfrec], 10* np.log10(2*np.mean(np.abs(ft_Nq)**2, axis=1)[bfrec]), ':c')
+plt.plot( ff[bfrec], 10* np.log10(2*np.abs(ft_Nn[bfrec])**2), ':r')
+plt.plot( ff[bfrec], 10* np.log10(2*np.abs(ft_Nq[bfrec])**2), ':c')
 plt.plot( np.array([ ff[bfrec][-1], ff[bfrec][-1] ]), plt.ylim(), ':k', label='BW', lw = 0.5  )
 
 plt.title('Señal muestreada por un ADC de {:d} bits - $\pm V_R= $ {:3.1f} V - q = {:3.3f} V'.format(B, Vf, q) )
@@ -153,16 +158,14 @@ plt.xlabel('Frecuencia [Hz]')
 axes_hdl = plt.gca()
 axes_hdl.legend()
 
-############
-Histograma
-############
+#############
+# Histograma
+#############
 
-plt.figure()
+plt.figure(3)
 bins = 10
-plt.hist(nqf.flatten()/(q/2), bins=bins)
-plt.hist(nq.flatten(), bins=2*bins)
-plt.plot( np.array([-q/2, -q/2, q/2, q/2]), np.array([0, N*R/bins, N*R/bins, 0]), '--r' )
-#plt.plot( np.array([-1/2, -1/2, 1/2, 1/2]), np.array([0, N*R/bins, N*R/bins, 0]), '--r' )
+plt.hist(nq.flatten()/(q), bins=bins)
+plt.plot( np.array([-1/2, -1/2, 1/2, 1/2]), np.array([0, N/bins, N/bins, 0]), '--r' )
 plt.title( 'Ruido de cuantización para {:d} bits - $\pm V_R= $ {:3.1f} V - q = {:3.3f} V'.format(B, Vf, q))
 
 plt.xlabel('Pasos de cuantización (q) [V]')
