835816f54466c0ed4a7af17d5894325a1d2e9b5f
Lucas <lukasmaru27@gmail.com>
2025-04-09 17:09:49 -0300

    Tuneo código

--- Ejercicios en clase/explicitacionDEventaneo_aperiodicidad_desparramo.py
+++ Ejercicios en clase/explicitacionDEventaneo_aperiodicidad_desparramo.py
@@ -19,7 +19,9 @@ def genera_grafica_tiempo_espectro(f0,fs,N):
     xx = A*np.sin( 2 * np.pi * f0 * tt ) # Declaro funcion senoidal
     plt.figure()
     plt.subplot(2,1,1)
-    plt.stem(tt, xx,basefmt=" ")
+    markerline, stemlines, baseline = plt.stem(tt, xx,basefmt=" ")
+    plt.setp(stemlines, linewidth=0.5)  # más fino
+    plt.setp(markerline, markersize=2,color='black')  # achico los puntos
     plt.title(f"Señal de {f0} Hz muestreada a {fs} Hz (tiempo)")
     plt.xlabel("Tiempo (s)")
     plt.ylabel("Amplitud")
@@ -29,8 +31,14 @@ def genera_grafica_tiempo_espectro(f0,fs,N):
     ff = np.linspace(0, (N-1)*df, N)# grilla de sampleo frecuencial
     bfrec = ff <= fs/2#vector booleano mitad True, mitad False
     plt.subplot(2,1,2)
-    plt.plot( ff[bfrec], np.abs(ft[bfrec]))
-    #plt.plot( ff[bfrec], 10* np.log10(2*np.abs(ft[bfrec])**2))
+    #plt.plot( ff[bfrec], np.abs(ft[bfrec]))
+    # plt.plot( ff[bfrec], 10* np.log10(2*np.abs(ft[bfrec])**2))
+    markerline, stemlines, baseline = plt.stem(ff[bfrec], 10 * np.log10(2 * np.abs(ft[bfrec])**2), basefmt=" ")
+    #captura los objetos que devuelve stem para por ajustarlos con setp
+    plt.setp(stemlines, linewidth=0.5)  # más fino
+    plt.setp(markerline, markersize=4,color='black')  # achico los puntos
+    # Línea fina que une los puntos (por encima del stem)
+    plt.plot( ff[bfrec], 10 * np.log10(2 * np.abs(ft[bfrec])**2), color='red', linewidth=2, label='Interpolación visual')    
     plt.title("Espectro (frecuencia)")
     plt.xlabel("Frecuencia (Hz)")
     plt.ylabel("Magnitud (dB)")
@@ -53,7 +61,7 @@ plt.legend()
 plt.tight_layout()
 plt.show()
 
-tt ,ts , N ,f0 ,fs = genera_grafica_tiempo_espectro(f0=4, fs=1200 , N=400)
+tt ,ts , N ,f0 ,fs = genera_grafica_tiempo_espectro(f0=4, fs=1200 , N=4000)
 
 window = np.ones(N)
 plt.subplot(2,1,1)
