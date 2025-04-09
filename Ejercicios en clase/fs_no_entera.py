1ef7f0e23c1132f8bc1dd020f31432f037905fb3
Lucas <lukasmaru27@gmail.com>
2025-04-08 23:28:39 -0300

    fs no entera
    Código para entender que pasa cuando fs no es entera

---
+++ Ejercicios en clase/fs_no_entera.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  8 19:14:02 2025
+
+@author: lmaru
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+def genera_grafica_tiempo_espectro(f0,fs,N):
+    #%% Datos de la simulación
+    ts = 1/fs # tiempo de muestreo
+    df = fs/N # resolución espectral
+    tt = np.linspace(0, (N-1)*ts, N).flatten()
+    "quiero que tt arranque en cero, termine en 0,999 y tenga N elementos"
+    A = math.sqrt(2) # proceso de normalización 
+    xx = A*np.sin( 2 * np.pi * f0 * tt ) # Declaro funcion senoidal
+    plt.figure()
+    plt.subplot(2,1,1)
+    plt.stem(tt, xx,basefmt=" ")
+    plt.title(f"Señal de {f0} Hz muestreada a {fs} Hz (tiempo)")
+    plt.xlabel("Tiempo (s)")
+    plt.ylabel("Amplitud")
+    plt.grid(True)
+    #plt.axis([min(tt), max(tt), -1, 1])#fijo el eje y para que sea igual en cada grafico
+    ft= 1/N*np.fft.fft(xx)#escalamiento no es importante (1/N)
+    ff = np.linspace(0, (N-1)*df, N)# grilla de sampleo frecuencial
+    bfrec = ff <= fs/2#vector booleano mitad True, mitad False
+    plt.subplot(2,1,2)
+    plt.plot( ff[bfrec], 10* np.log10(2*np.abs(ft[bfrec])**2))
+    plt.title("Espectro (frecuencia)")
+    plt.xlabel("Frecuencia (Hz)")
+    plt.ylabel("Magnitud (dB)")
+    plt.grid(True)
+
+#genera_grafica_tiempo_espectro(1, 1000,1000)
+#genera_grafica_tiempo_espectro(1, 1000.5,1000)
+genera_grafica_tiempo_espectro(1, 50 , 100)
+genera_grafica_tiempo_espectro(1, 51.5, 100)
