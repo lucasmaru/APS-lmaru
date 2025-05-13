afdb4ad0ecd7f380d5cbcd3b44bd1f4d708fb923
Lucas <lukasmaru27@gmail.com>
2025-05-13 00:10:44 -0300

    Agrego las señales sin ruido
    Incorpora las nuevas señales filtradas y distingo un código con las señales originales y otro con las señales filtradas

--- Tareas_semanales/TS5/TS5_sin_ruido.py
+++ Tareas_semanales/TS5/TS5_sin_ruido.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Created on Mon May 12 20:52:01 2025
+
+@author: lmaru
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 12 19:29:32 2025
+
+@author: lmaru
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
 Created on Thu May  8 21:46:19 2025
 
 @author: lmaru
@@ -42,9 +58,7 @@ def blackman_tukey(x, fs, M=None):
 
 ##Lectura##
 fs_ecg = 1000 # Hz, la frecuencia de muestreo a la que fue sampleada la señal del archivo
-mat_struct = sio.loadmat('./ECG_TP4.mat') #Leo el archivo
-ecg_one_lead = mat_struct['ecg_lead'].flatten() #Tomo la parte que quiero
-ecg_one_lead = ecg_one_lead[:12000] #Nos quedamos con una parte del espectro limpia pero significativa
+ecg_one_lead = np.load('ecg_sin_ruido.npy')
 N_ecg = len(ecg_one_lead)
 
 ##Welch##
@@ -74,7 +88,7 @@ f_ecg_bt, PSD_bt, PSD_db_bt = blackman_tukey(ecg_one_lead, fs=fs_ecg, M=N_ecg//5
 PSD_db_bt_norm = PSD_db_bt - np.max(PSD_db_bt) #Normalizo respecto al máximo (pico en 0 dB)
 
 ##Estimación ancho de banda Welch##
-porcentaje = 0.95
+porcentaje = 0.98
 pot_total_welch =np.sum(PSD_ecg)
 pot_acumulada_welch = np.cumsum(PSD_ecg) / pot_total_welch  # ahora ya es proporción
 i = np.argmax(pot_acumulada_welch >= porcentaje)  # primer índice que supera el %
@@ -118,7 +132,7 @@ los tejidos."""
 
 ##Lectura##
 fs_ppg = 400 # Hz, la frecuencia de muestreo a la que fue sampleada la señal del archivo
-ppg = np.genfromtxt('PPG.csv', delimiter=',', skip_header=1)  # Omitir la cabecera si existe
+ppg = np.load('ppg_sin_ruido.npy')
 N_ppg = len(ppg)
 
 ##Welch##
@@ -134,7 +148,7 @@ f_ppg_bt, PSD_bt, PSD_db_bt = blackman_tukey(ppg, fs=fs_ppg) #Blackman-Tukey
 PSD_db_bt_norm = PSD_db_bt - np.max(PSD_db_bt) #Normalizo respecto al máximo (pico en 0 dB)
 
 ##Estimación ancho de banda Welch##
-porcentaje = 0.95
+porcentaje = 0.98
 pot_total_welch =np.sum(PSD_ppg)
 pot_acumulada_welch = np.cumsum(PSD_ppg) / pot_total_welch  # ahora ya es proporción
 i = np.argmax(pot_acumulada_welch >= porcentaje)  # primer índice que supera el %
@@ -169,54 +183,60 @@ plt.grid(True)
 #%%#################
 # Lectura de audio #
 ####################
+# Archivos de audio a procesar
+archivos = ['la cucaracha.wav', 'prueba psd.wav', 'silbido.wav']
+titulos = ['La Cucaracha', 'Prueba PSD', 'Silbido']
 
-##Lectura##
-#fs_audio, wav_data = sio.wavfile.read('la cucaracha.wav') #esta funciòn devuelve la fs
-fs_audio, wav_data = sio.wavfile.read('prueba psd.wav')
-#fs_audio, wav_data = sio.wavfile.read('silbido.wav')
-N_audio = len(wav_data)
-
-##Welch##
-nperseg = N_audio // 5  #cantidad de segmentos
-noverlap = nperseg//2 #solapamiento
-f_audio, PSD_audio = sig.welch(wav_data, fs_audio, window='hamming',
-                    nperseg=nperseg, noverlap=noverlap, detrend= 'linear')
-PSD_audio_db = 10 * np.log10(PSD_audio) #Paso a dB
-PSD_audio_db_norm = PSD_audio_db - np.max(PSD_audio_db) #Normalizo respecto al máximo (pico en 0 dB)
-
-##Blackman-Tukey##
-f_bt_audio, PSD_bt, PSD_db_bt = blackman_tukey(wav_data, fs=fs_audio)
-PSD_db_bt_norm = PSD_db_bt - np.max(PSD_db_bt) #Normalizo respecto al máximo (pico en 0 dB)
-
-##Estimación ancho de banda Welch##
-porcentaje = 0.98
-pot_total_welch =np.sum(PSD_audio)
-pot_acumulada_welch = np.cumsum(PSD_audio) / pot_total_welch  # ahora ya es proporción
-i = np.argmax(pot_acumulada_welch >= porcentaje)  # primer índice que supera el %
-frec_buscada_welch = f_audio[i]
-
-##Estimación ancho de banda B-T##
-pot_total_bt = np.sum(PSD_bt)
-pot_acumulada_bt = np.cumsum(PSD_bt) / pot_total_bt  # ahora ya es proporción
-i = np.argmax(pot_acumulada_bt >= porcentaje)  # primer índice que supera el %
-frec_buscada_bt = f_bt_audio[i]
-
-plt.figure(figsize=(10, 6))
-
-plt.subplot(2, 1, 1)
-plt.plot(f_audio, PSD_audio_db_norm)
-plt.xlabel('Frecuencia [Hz]')
-plt.ylabel('Densidad espectral normalizada [dB/Hz]')
-plt.title('PSD del audio en dB (método de Welch)')
-plt.vlines(x=frec_buscada_welch,ymin=-90,ymax=0,colors='black',linestyles='dashdot',label=f'BW {porcentaje *100:.0f}% {frec_buscada_welch:.2f} hz')
-plt.legend()
-plt.grid(True)
-
-plt.subplot(2, 1, 2)
-plt.plot(f_bt_audio, PSD_db_bt_norm)
-plt.xlabel('Frecuencia [Hz]')
-plt.ylabel('PSD [dB/Hz]')
-plt.title('PSD del audio en dB (método de Blackman-Tukey)')
-plt.vlines(x=frec_buscada_bt,ymin=-90,ymax=0,colors='black',linestyles='dashdot',label=f'BW {porcentaje *100:.0f}% {frec_buscada_welch:.2f} hz')
-plt.legend()
-plt.grid(True)
+for j, archivo in enumerate(archivos):
+    fs, x = sio.wavfile.read(archivo) # Carga de señal
+    x = x.astype(np.float64)
+    ##Lectura##
+    fs_audio, wav_data = sio.wavfile.read(archivo) #esta funciòn devuelve la fs
+    N_audio = len(wav_data)
+    
+    ##Welch##
+    nperseg = N_audio // 50  #cantidad de segmentos
+    noverlap = nperseg//2 #solapamiento
+    f_audio, PSD_audio = sig.welch(wav_data, fs_audio, window='hamming',
+                        nperseg=nperseg, noverlap=noverlap)
+    PSD_audio_db = 10 * np.log10(PSD_audio) #Paso a dB
+    PSD_audio_db_norm = PSD_audio_db - np.max(PSD_audio_db) #Normalizo respecto al máximo (pico en 0 dB)
+    
+    ##Blackman-Tukey##
+    f_bt_audio, PSD_bt, PSD_db_bt = blackman_tukey(wav_data, fs=fs_audio)
+    PSD_db_bt_norm = PSD_db_bt - np.max(PSD_db_bt) #Normalizo respecto al máximo (pico en 0 dB)
+    
+    ##Estimación ancho de banda Welch##
+    porcentaje = 0.98
+    pot_total_welch =np.sum(PSD_audio)
+    pot_acumulada_welch = np.cumsum(PSD_audio) / pot_total_welch  # ahora ya es proporción
+    i = np.argmax(pot_acumulada_welch >= porcentaje)  # primer índice que supera el %
+    frec_buscada_welch = f_audio[i]
+    
+    ##Estimación ancho de banda B-T##
+    pot_total_bt = np.sum(PSD_bt)
+    pot_acumulada_bt = np.cumsum(PSD_bt) / pot_total_bt  # ahora ya es proporción
+    i = np.argmax(pot_acumulada_bt >= porcentaje)  # primer índice que supera el %
+    frec_buscada_bt = f_bt_audio[i]
+    
+    ##Visualización##
+    plt.figure(figsize=(10, 6))
+    
+    plt.subplot(2, 1, 1)
+    plt.plot(f_audio, PSD_audio_db_norm)
+    plt.xlabel('Frecuencia [Hz]')
+    plt.ylabel('Densidad espectral normalizada [dB/Hz]')
+    plt.title(f'PSD de {titulos[j]} – Welch')
+    plt.vlines(x=frec_buscada_welch,ymin=-90,ymax=0,colors='black',linestyles='dashdot',label=f'BW {porcentaje *100:.0f}% {frec_buscada_welch:.2f} hz')
+    plt.legend()
+    plt.grid(True)
+    
+    plt.subplot(2, 1, 2)
+    plt.plot(f_bt_audio, PSD_db_bt_norm)
+    plt.xlabel('Frecuencia [Hz]')
+    plt.ylabel('PSD [dB/Hz]')
+    #plt.title('PSD del audio en dB (método de Blackman-Tukey)')
+    plt.title(f'PSD de {titulos[j]} – Blackman-Tukey')
+    plt.vlines(x=frec_buscada_bt,ymin=-90,ymax=0,colors='black',linestyles='dashdot',label=f'BW {porcentaje *100:.0f}% {frec_buscada_welch:.2f} hz')
+    plt.legend()
+    plt.grid(True)
